b2a374d8bbfbe6b800d702360101b3c40f5fdde0
leon_id075 <grebenukleonid075@gmail.com>
2024-03-24 09:30:18 +0300

    update train_mle.py

--- train_mle.py
+++ train_mle.py
@@ -107,50 +107,6 @@ def main():
                                                      min_lr=args.lr_decay_min)
     optimizer.step()
 
-    # def generate_one(model, mute=False, cnt=None):
-    #     """
-    #     inverse flow to generate one molecule
-    #     Args:
-    #         temp: temperature of normal distributions, we sample from (0, temp^2 * I)
-    #     """
-    #     generate_start_t = time()
-    #     num2bond = {0: Chem.rdchem.BondType.SINGLE, 1: Chem.rdchem.BondType.DOUBLE, 2: Chem.rdchem.BondType.TRIPLE}
-    #     num2bond_symbol = {0: '=', 1: '==', 2: '==='}
-    #     num2atom = {0: 6, 1: 7, 2: 8, 3: 9, 4: 15, 5: 16, 6: 17, 7: 35, 8: 53}
-    #     num2symbol = {0: 'C', 1: 'N', 2: 'O', 3: 'F', 4: 'P', 5: 'S', 6: 'Cl', 7: 'Br', 8: 'I'}
-    #     is_continue = True
-    #     mol = None
-    #     total_resample = 0
-    #     batch_size = 1
-    #     # Generating
-    #     z = sample_z(model, batch_size=1)
-    #     A, X = model.reverse(z, model.x_size) # For QM9: [16,9,9,5], [16,9,5], [16,8]-[B,z_dim]
-    #     X = F.softmax(X, dim=2)
-    #     mols = [construct_mol(x_elem, adj_elem, args.atomic_num_list)
-    #             for x_elem, adj_elem in zip(X, A)]
-    #     pure_valid = 0
-    #     smiles = ''
-    #     num_atoms = -1
-    #     for mol in mols:
-    #         assert mol is not None, 'mol is None...'
-    #         final_valid = env.check_chemical_validity(mol)
-    #         valency_valid = env.check_valency(mol)
-
-    #         if final_valid is False or valency_valid is False:
-    #             print('Warning: use valency check during generation but the final molecule is invalid!!!')
-    #             continue
-    #         num_atoms = mol.GetNumAtoms()
-    #         num_bonds = mol.GetNumBonds()
-    #         smiles = Chem.MolToSmiles(mol)
-
-    #         if total_resample == 0:
-    #             pure_valid = 1.0
-    #         if not mute:
-    #             cnt = str(cnt) if cnt is not None else ''
-    #             print('smiles%s: %s | #atoms: %d | #bonds: %d | #resample: %.5f | time: %.5f |' % (
-    #                 cnt, smiles, num_atoms, num_bonds, total_resample, time() - generate_start_t))
-    #     return smiles, A, X, pure_valid, num_atoms
-
     def train(model):
         for epoch in range(1+start_epoch, args.epochs + 1 - start_epoch):
             batch_g_losses = []
